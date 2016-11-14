20d0eabe10a9849b72eceefb88f9e6b0bb1dfec7
SergeyKolenko <venty80@gmail.com>
2016-11-14 13:40:16 +0200

    code review!

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+# TODO: 'Use ~> or => or remove versions'
 gem 'rails',                   '5.0.0.1'
 gem 'bcrypt',                  '3.1.11'
 gem 'carrierwave',             '0.11.2'
@@ -17,6 +18,7 @@ gem 'jbuilder',                '2.4.1'
 group :development, :test do
   gem 'sqlite3', '1.3.11'
   gem 'byebug',  '9.0.0', platform: :mri
+  gem 'faker',                   '1.6.6'
 end
 
 group :development do
@@ -31,12 +33,12 @@ group :test do
   gem 'minitest-reporters',       '1.1.9'
   gem 'guard',                    '2.13.0'
   gem 'guard-minitest',           '2.4.4'
-  gem 'faker',                   '1.6.6'
 end
 
 group :production do
-  gem 'pg',   '0.18.4'
+  # gem 'pg',   '0.18.4'
 end
 
+#FIXME: Remove this!!!!
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
+  get    '/help',    to: 'static_pages#help' # FIXME: into 'controller' section!
+  get    '/about',   to: 'static_pages#about' # FIXME: into 'controller' section!
+  get    '/contact', to: 'static_pages#contact' # FIXME: into 'controller' section!
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  # EXAMPLE
+  # controller :static_pages do
+  #   get :help
+  #   ...
+  # end
+
   resources :users do
     member do
       get :following, :followers
     end
   end
+  get '/signup',  to: 'users#new'
+
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
