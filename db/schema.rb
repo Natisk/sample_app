521c84e25b85cc876c164f82ff66d58b6ed1a8d5
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-11-17 18:27:56 +0200

    Added comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116112649) do
+ActiveRecord::Schema.define(version: 20161117162243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "micropost_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["created_at"], name: "index_comments_on_created_at", using: :btree
+    t.index ["micropost_id"], name: "index_comments_on_micropost_id", using: :btree
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -61,5 +71,6 @@ ActiveRecord::Schema.define(version: 20161116112649) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "microposts"
   add_foreign_key "microposts", "users"
 end
