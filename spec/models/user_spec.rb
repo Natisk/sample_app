2438b83248d26ea02b21ec2fc02e8d9f1c1b588e
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-12-01 13:43:47 +0200

    Devise test helper set only for controllers specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe User, type: :model do
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  # @request.env['devise.mapping'] = Devise.mappings[:user]
 
   context 'model connection' do
-    it { should have_many(:articles) }
+    it { should have_many(:microposts) }
 
   end
 
-  context 'User db column' do
-    it { should have_db_column(:id).of_type(:integer)}
-    it { should have_db_column(:email).of_type(:string).with_options(default: "", null: false)}
-    it { should have_db_column(:sign_in_count).of_type(:integer).with_options(:default => 0)}
-    it { should have_many(:articles) }
-  end
+  # context 'User db column' do
+  #  it { should have_db_column(:id).of_type(:integer)}
+  #  it { should have_db_column(:email).of_type(:string).with_options(default: "", null: false)}
+  #  it { should have_db_column(:sign_in_count).of_type(:integer).with_options(:default => 0)}
+  #  it { should have_many(:articles) }
+  # end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers
-  config.include Warden::Test::Helpers
+  config.include Devise::TestHelpers, type: :controller
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
