bb45d188d378f58f378a5cf7be77844b241d1429
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-12-09 13:49:34 +0200

    Fixes for data recieved as collback from twitter via omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,32 +1,34 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+  before_action :set_user, only: [:facebook, :twitter]
 
+  def facebook
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      flash[:notice] = 'User with this email already registered'
+      flash[:notice] = 'Current email already was registered'
       redirect_to new_user_registration_url
     end
   end
 
   def twitter
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
     else
-      session['devise.twitter_data'] = request.env['omniauth.auth']
+      session['devise.twitter_data'] = request.env['omniauth.auth'].delete_if('extra')
+      flash[:notice] = 'Current email already was registered'
       redirect_to new_user_registration_url
     end
   end
 
   def failure
     redirect_to root_path
+  end
+
+  private
+  def set_user
+    @user = User.from_omniauth(request.env['omniauth.auth'])
   end
 end
