6586cece99a5907abb0506a6c3ca1a8445ecd1ae
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-12-09 16:50:12 +0200

    Tring to configure twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,11 +51,7 @@ class User < ApplicationRecord
   def self.twitter_omniauth(auth)
     where(provider: auth.provider, twitter_uid: auth.uid).first_or_create do |user|
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.name = auth.info.name
     end
   end
 end
