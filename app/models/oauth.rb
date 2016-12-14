2dd5d1b0cf26a8260af83532a086275d9ef45a28
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2016-12-14 13:58:40 +0200

    Social buttons styling

--- app/models/oauth.rb
+++ app/models/oauth.rb
@@ -1,5 +1,6 @@
 class Oauth < ApplicationRecord
   belongs_to :user, inverse_of: :oauths
-  # validates :uid, :provider, :user, presence: true
-  validates :uid, uniqueness: { scope: :provider }
+  validates :user, presence: true
+  validates :uid, uniqueness: { scope: :provider }, presence: true
+  validates :provider, presence: true, inclusion: { in: PROVIDERS }
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,1 @@
+PROVIDERS = %w[facebook twitter]
